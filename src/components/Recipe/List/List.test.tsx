ef190387d911b3042f4fbe059f33885aa6cb454d
Ivan Kuzel <ivan.kuzel@travelperk.com>
2021-07-25 15:30:59 -0300

    Refactored test for recipe list component tu use router

--- src/components/Recipe/List/List.test.tsx
+++ src/components/Recipe/List/List.test.tsx
@@ -1,5 +1,10 @@
 import React from 'react'
 import { render, fireEvent, waitFor, screen } from '@testing-library/react'
+import { RenderAndHistory } from 'utils/test/types'
+import { Recipe } from 'data/recipes/types'
+import { createMemoryHistory } from 'history'
+import { Router } from 'react-router-dom'
+
 import RecipeList from './List'
 
 const recipes = [
@@ -15,10 +20,25 @@ const recipes = [
     description: 'Creamy lemon risotto',
     ingredients: [{ name: 'Carnaroli Rice' }, { name: 'Chicken Broth' }, { name: 'Parmesano Cheese' }],
   },
-]
+];
+
+interface Props {
+  recipes: Recipe[];
+}
+
+function renderRecipeList(props?: Props): RenderAndHistory {
+  const history = createMemoryHistory()
+  const utils = render(
+    <Router history={history}>
+      <RecipeList {...props} />
+    </Router>
+  )
+
+  return { ...utils, history }
+}
 
 test('<RecipeList> with recipes', async () => {
-  render(<RecipeList recipes={recipes} />)
+  renderRecipeList({ recipes });
   expect(await screen.findByText("All recipes")).toBeInTheDocument();
   expect(await screen.findByText("Chocolate cookies")).toBeInTheDocument();
   expect(await screen.findByText("The best fudgy cookies you will ever try")).toBeInTheDocument();
@@ -27,6 +47,6 @@ test('<RecipeList> with recipes', async () => {
 });
 
 test('<RecipeList> without recipes', async () => {
-  render(<RecipeList />)
+  renderRecipeList();
   expect(await screen.findByText("No recipes.")).toBeInTheDocument();
 });
